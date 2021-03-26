25e881b0b2ea78795b0f025e502cc2f61d811cfb
Carlosxc-dev <61745249+Carlosxc-dev@users.noreply.github.com>
2021-03-26 13:41:48 -0300

    up

--- exercicios nivel 2/009-cores.py
+++ exercicios nivel 2/009-cores.py
@@ -3,5 +3,5 @@
 #print('\033[4;37;43mola mundo!\033[m')
 #print('\033[7;32;44mola mundo!\033[m')
 
-print('{}carlos henrique cruz xavier{}'.format('\033[0;37;40m', '\033[m')) # mais facil de fazer
+print('{}carlos henrique cruz xavier{}'.format('\033[0;37;46m', '\033[m')) # mais facil de fazer
 #pesquisar color rize 
